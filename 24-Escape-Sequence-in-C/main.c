e914d83048028a74bcc7380e06183ff38cc348aa
Matt Johnson <matthewjohnson@snarkydata.com>
2023-06-05 18:46:21 -0700

    Adding 24-Escape-Sequence-in-C files and directories

---
+++ 24-Escape-Sequence-in-C/main.c
@@ -0,0 +1,17 @@
+/*
+    Author: Matt Johnson
+    Subject: escape sequences in C
+    Purpose: program uses \n and \r
+    Date: 06/05/2023
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    printf("Hello world!\r");
+    printf("Bye !");
+
+    return 0;
+}
