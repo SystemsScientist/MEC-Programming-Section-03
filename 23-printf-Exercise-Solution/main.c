4e92ba3f74ed2dc7ae6789a616cddef23aadbab2
Matt Johnson <matthewjohnson@snarkydata.com>
2023-06-05 18:06:41 -0700

    Adding 23-printf-Exercise-Solution files and directories

---
+++ 23-printf-Exercise-Solution/main.c
@@ -0,0 +1,27 @@
+/*
+    Author: Matt Johnson
+    Subject: printf exercise solution
+    Purpose: program prints an additional
+             print statement
+    Date: 06/04/2023
+
+    Requirements:
+
+        - add an additional printf statement
+        - display "Today is a great day!" in
+          the additional printf statement
+
+        - Output
+            Hello, World!
+            Today is a great day!
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    printf("Hello, world!\n");
+    printf("Today is a great day!\n");
+    return 0;
+}
