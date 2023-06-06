75448191a74dacc66e42be4a44513454c0f49daf
Matt Johnson <matthewjohnson@snarkydata.com>
2023-06-05 17:30:33 -0700

    Adding 22-printf-and-Hello-World files and directories

---
+++ 22-printf-and-Hello-World/main.c
@@ -0,0 +1,16 @@
+/*
+    Author: Matt Johnson
+    Subject: "Hello, World!" program
+    Purpose: program prints "Hello, World!"
+    Date: 06/04/2023
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    printf("Hello, World!\n");
+
+    return 0;
+}
