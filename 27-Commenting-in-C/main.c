586e76e6a3cf0ba1e84c4fde6937e1c62f5ef9bd
Matt Johnson <matthewjohnson@snarkydata.com>
2023-06-06 17:27:04 -0700

    Adding 27-Commenting-in-C files and directories

---
+++ 27-Commenting-in-C/main.c
@@ -0,0 +1,20 @@
+/*
+    Author: Matt Johnson
+    Subject: commenting in C
+    Purpose:
+    Date: 06/06/2023
+
+    Requirements:
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    printf("Hello World !\r");          // This is a comment
+    printf("Today is a great day\n");   // This is a comment
+
+    return 0;
+}
