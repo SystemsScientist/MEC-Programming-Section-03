fd5a58d2f6f022eb541d30110549ea961c8779e5
Matt Johnson <matthewjohnson@snarkydata.com>
2023-06-06 17:07:19 -0700

    Adding 25-printf-Exercise-Solution files and directories

---
+++ 25-printf-Exercise-Solution/main.c
@@ -0,0 +1,24 @@
+/*
+    Author: Matt Johnson
+    Subject: escape sequences in C exercise solution
+    Purpose:
+    Date: 06/05/2023
+
+    Requirements:
+
+    - program displays the following
+
+        David says , " Programming is fun !"
+        Good day !
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("David says , \" Programming is fun !\"\n");
+    printf("Good day !\n");
+
+    return 0;
+}
